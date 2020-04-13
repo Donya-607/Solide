ce5a4fb4fb3a9f246559414775d4e40a61468158
Donya <alias>
2020-04-13 19:58:10 +0900

    敵の基底クラスや簡単な具象クラスの実装を書いた。

--- Solide/Code/Enemy.cpp
+++ Solide/Code/Enemy.cpp
@@ -1,2 +1,302 @@
 #include "Enemy.h"
 
+#include <vector>
+
+#include "Donya/Loader.h"
+#include "Donya/Useful.h"
+
+namespace
+{
+	constexpr size_t KIND_COUNT = scast<size_t>( Enemy::Kind::KindCount );
+	constexpr const char *MODEL_DIRECTORY = "./Data/Models/Enemy/";
+	constexpr const char *MODEL_EXTENSION = "./Data/Models/Enemy/";
+	constexpr const char *MODEL_NAMES[KIND_COUNT]
+	{
+		"Run",
+	};
+
+	static std::vector<std::shared_ptr<Enemy::ModelParam>> modelPtrs{};
+
+	bool LoadModels()
+	{
+		// Already has loaded.
+		if ( !modelPtrs.empty() ) { return true; }
+		// else
+
+		modelPtrs.resize( KIND_COUNT );
+
+		Donya::Loader loader{};
+		auto Load = [&loader]( const std::string &filePath, Enemy::ModelParam *pDest )->bool
+		{
+			loader.ClearData();
+
+			bool  result  = loader.Load( filePath );
+			if ( !result ) { return false; }
+			// else
+
+			const auto &source = loader.GetModelSource();
+			pDest->model = Donya::Model::SkinningModel::Create( source, loader.GetFileDirectory() );
+			pDest->motionHolder.AppendSource( source );
+
+			return pDest->model.WasInitializeSucceeded();
+		};
+
+		bool result		= true;
+		bool succeeded	= true;
+
+		std::string filePath{};
+		const std::string prefix = MODEL_DIRECTORY;
+		for ( size_t i = 0; i < KIND_COUNT; ++i )
+		{
+			filePath = prefix + MODEL_NAMES[i] + MODEL_EXTENSION;
+			if ( !Donya::IsExistFile( filePath ) )
+			{
+				const std::string outputMsgBase{ "Error : The model file does not exist. That is : " };
+				Donya::OutputDebugStr( ( outputMsgBase + "[" + filePath + "]" + "\n" ).c_str() );
+				continue;
+			}
+			// else
+
+			auto &pModel = modelPtrs[i];
+			pModel = std::make_shared<Enemy::ModelParam>();
+			result = Load( filePath, &( *pModel ) ); // std::shared_ptr<T> -> T -> T *
+			if ( !result )
+			{
+				const std::wstring errMsgBase{ L"Failed : Loading a model. That is : " };
+				const std::wstring errMsg = errMsgBase + Donya::MultiToWide( filePath );
+				_ASSERT_EXPR( 0, errMsg.c_str() );
+
+				succeeded = false;
+			}
+		}
+
+		if ( !succeeded )
+		{
+			modelPtrs.clear();
+			return false;
+		}
+		// else
+		return true;
+	}
+
+	bool IsOutOfRange( Enemy::Kind kind )
+	{
+		return ( scast<int>( kind ) < 0 || Enemy::Kind::KindCount <= kind ) ? true : false;
+	}
+	const std::shared_ptr<Enemy::ModelParam> GetModelPtr( Enemy::Kind kind )
+	{
+		if ( modelPtrs.empty() )
+		{
+			_ASSERT_EXPR( 0, L"Error : The models were not initialized.!" );
+			return nullptr;
+		}
+		// else
+		if ( IsOutOfRange( kind ) )
+		{
+			_ASSERT_EXPR( 0, L"Error : Model subscript out of range!" );
+			return nullptr;
+		}
+		// else
+		return modelPtrs[scast<int>( kind )];
+	}
+}
+
+
+namespace Enemy
+{
+	bool LoadResources()
+	{
+		return LoadModels();
+	}
+
+#if USE_IMGUI
+	void InitializeParam::ShowImGuiNode( const std::string &nodeCaption )
+	{
+		if ( !ImGui::TreeNode( nodeCaption.c_str() ) ) { return; }
+		// else
+
+		ImGui::DragFloat3( u8"初期のワールド座標", &wsPos.x );
+
+		Donya::Vector3 lookDir = orientation.LocalFront();
+		ImGui::SliderFloat3( u8"初期の前方向", &lookDir.x, -1.0f, 1.0f );
+		orientation = Donya::Quaternion::LookAt( Donya::Vector3::Front(), lookDir.Unit(), Donya::Quaternion::Freeze::Up );
+
+		ImGui::TreePop();
+	}
+#endif // USE_IMGUI
+
+
+	Donya::Vector3 MoveParam::CalcMoveDirection( const Donya::Vector3 moverPos, const Donya::Vector3 &targetPos ) const
+	{
+		if ( !alignToTarget ) { return direction; }
+		// else
+
+		const	Donya::Vector3 toTarget		= targetPos - moverPos;
+		const	Donya::Vector3 projection	= direction * Donya::Dot( toTarget.Unit(), direction );
+		return	projection;
+	}
+#if USE_IMGUI
+	void MoveParam::ShowImGuiNode( const std::string &nodeCaption )
+	{
+		if ( !ImGui::TreeNode( nodeCaption.c_str() ) ) { return; }
+		// else
+
+		ImGui::DragFloat( u8"移動速度（フレームあたり）",		&speed,			0.01f );
+		ImGui::DragFloat( u8"移動範囲制限（ゼロで無制限）",	&rangeLimit,	0.01f );
+
+		ImGui::SliderFloat3( u8"移動方向（単位ベクトル）",		&direction.x, -1.0f, 1.0f );
+		direction.Normalize();
+		ImGui::Checkbox( u8"自機と同列にあわせるか",			&alignToTarget );
+
+		if ( ImGui::TreeNode( u8"正面方向の設定" ) )
+		{
+			if ( ImGui::RadioButton( u8"移動方向を向く",		lookDirection == LookDirection::MoveDirection ) )
+			{ lookDirection = LookDirection::MoveDirection; }
+			if ( ImGui::RadioButton( u8"自機の方向を向く",	lookDirection == LookDirection::Target ) )
+			{ lookDirection = LookDirection::Target; }
+
+			ImGui::TreePop();
+		}
+
+		ImGui::TreePop();
+	}
+#endif // USE_IMGUI
+
+
+	void Base::Draw( RenderingHelper *pRenderer )
+	{
+		if ( !pModelParam ) { return; }
+		// else
+
+		Donya::Model::Constants::PerModel::Common constant{};
+		constant.drawColor		= Donya::Vector4{ 1.0f, 1.0f, 1.0f, 1.0f };
+		constant.worldMatrix	= CalcWorldMatrix( /* useForHitBox = */ false );
+		pRenderer->UpdateConstant( constant );
+		pRenderer->ActivateConstantModel();
+
+		pRenderer->Render( pModelParam->model, pose );
+
+		pRenderer->DeactivateConstantModel();
+	}
+	void Base::DrawHitBox( RenderingHelper *pRenderer, const Donya::Vector4x4 &VP )
+	{
+		Donya::Model::Cube::Constant constant{};
+		constant.matWorld		= CalcWorldMatrix( /* useForHitBox = */ true );
+		constant.matViewProj	= VP;
+		constant.drawColor		= Donya::Vector4{ 1.0f, 1.0f, 1.0f, 0.5f };
+		pRenderer->ProcessDrawingCube( constant );
+	}
+	Donya::Vector4x4 Base::CalcWorldMatrix( bool useForHitBox ) const
+	{
+		// Scales are 1.0f.
+		Donya::Vector4x4 W{};
+
+		if ( useForHitBox )
+		{
+			W._11 *= 2.0f;
+			W._22 *= 2.0f;
+			W._33 *= 2.0f;
+			W._41 = pos.x;
+			W._42 = pos.y;
+			W._43 = pos.z;
+			return W;
+		}
+		// else
+
+		W = orientation.RequireRotationMatrix();
+		W._41 = pos.x;
+		W._42 = pos.y;
+		W._43 = pos.z;
+		return W;
+	}
+
+
+	void Straight::Init( const InitializeParam &argInitializer )
+	{
+		initializer	= argInitializer;
+
+		pos			= initializer.wsPos;
+		orientation	= initializer.orientation;
+
+		animator.ResetTimer();
+
+		pModelParam	= GetModelPtr( Kind::Straight );
+		if ( pModelParam )
+		{
+			const auto &initialMotion = pModelParam->motionHolder.GetMotion( 0 );
+			animator.SetRepeatRange( initialMotion );
+			pose.AssignSkeletal
+			(
+				animator.CalcCurrentPose
+				(
+					initialMotion
+				)
+			);
+		}
+
+		velocity			= 0.0f;
+		moveDistanceSum		= 0.0f;
+		nowMoveToPositive	= true;
+	}
+	void Straight::Update( float elapsedTime, const Donya::Vector3 &targetPos )
+	{
+		AssignVelocity( targetPos );
+		AssignOrientation( targetPos );
+
+		moveDistanceSum += velocity.Length();
+		if ( moveParam.rangeLimit <= moveDistanceSum )
+		{
+			// To inside of rangeLimit.
+			const Donya::Vector3 movedDir = Donya::Vector3{ pos - initializer.wsPos }.Unit();
+			const Donya::Vector3 correctMovedPos = initializer.wsPos + movedDir * moveParam.rangeLimit;
+			velocity = correctMovedPos - pos;
+
+			moveDistanceSum		= -moveParam.rangeLimit;
+			nowMoveToPositive	= !nowMoveToPositive;
+		}
+	}
+	void Straight::PhysicUpdate()
+	{
+		pos += velocity;
+	}
+	Donya::Vector3 Straight::CalcNowMoveDirection( const Donya::Vector3 &targetPos ) const
+	{
+		const float moveSign = ( nowMoveToPositive ) ? 1.0f : -1.0f;
+		const Donya::Vector3 moveDirection = moveParam.CalcMoveDirection( pos, targetPos );
+		return moveDirection * moveSign;
+	}
+	void Straight::AssignVelocity( const Donya::Vector3 &targetPos )
+	{
+		const Donya::Vector3 moveDirection = CalcNowMoveDirection( targetPos );
+		velocity = moveDirection * moveParam.speed;
+	}
+	void Straight::AssignOrientation( const Donya::Vector3 &targetPos )
+	{
+		switch ( moveParam.lookDirection )
+		{
+		case MoveParam::LookDirection::MoveDirection:
+			{
+				const Donya::Vector3 moveDirection = CalcNowMoveDirection( targetPos );
+				orientation = Donya::Quaternion::LookAt
+				(
+					Donya::Vector3::Front(),
+					moveDirection.Unit(),
+					Donya::Quaternion::Freeze::Up
+				);
+			}
+			return;
+		case MoveParam::LookDirection::Target:
+			{
+				const Donya::Vector3 toTarget = targetPos - pos;
+				orientation = Donya::Quaternion::LookAt
+				(
+					Donya::Vector3::Front(),
+					toTarget.Unit(),
+					Donya::Quaternion::Freeze::Up
+				);
+			}
+			return;
+		default: return;
+		}
+	}
+}
--- Solide/Code/Enemy.h
+++ Solide/Code/Enemy.h
@@ -1,6 +1,196 @@
 #pragma once
 
+#include <string>
+#include <memory>
+
+#undef max
+#undef min
+#include <cereal/types/polymorphic.hpp>
+
+#include "Donya/Model.h"
+#include "Donya/ModelMotion.h"
+#include "Donya/ModelPose.h"
+#include "Donya/Quaternion.h"
+#include "Donya/Serializer.h"
+#include "Donya/UseImGui.h"
+#include "Donya/Vector.h"
+
+#include "Renderer.h"
+
 namespace Enemy
 {
+	enum class Kind
+	{
+		Straight,
 
+		KindCount
+	};
+
+	bool LoadResources();
+	
+	
+	struct InitializeParam
+	{
+		Donya::Vector3		wsPos;
+		Donya::Quaternion	orientation;
+	private:
+		friend class cereal::access;
+		template<class Archive>
+		void serialize( Archive &archive, std::uint32_t version )
+		{
+			archive
+			(
+				CEREAL_NVP( wsPos		),
+				CEREAL_NVP( orientation	)
+			);
+
+			if ( 1 <= version )
+			{
+				// archive( CEREAL_NVP( x ) );
+			}
+		}
+	public:
+	#if USE_IMGUI
+		void ShowImGuiNode( const std::string &nodeCaption );
+	#endif // USE_IMGUI
+	};
+	
+	struct MoveParam
+	{
+		enum class LookDirection
+		{
+			MoveDirection,
+			Target
+		};
+		LookDirection	lookDirection;
+		float			speed			= 0.0f;	// The maximum speed per frame.
+		float			rangeLimit		= 0.0f;	// 0.0f means limitless.
+		Donya::Vector3	direction;				// Unit vector.
+		bool			alignToTarget	= false;
+	private:
+		friend class cereal::access;
+		template<class Archive>
+		void serialize( Archive &archive, std::uint32_t version )
+		{
+			archive
+			(
+				CEREAL_NVP( lookDirection	),
+				CEREAL_NVP( speed			),
+				CEREAL_NVP( rangeLimit		),
+				CEREAL_NVP( direction		),
+				CEREAL_NVP( alignToTarget	)
+			);
+
+			if ( 1 <= version )
+			{
+				// archive( CEREAL_NVP( x ) );
+			}
+		}
+	public:
+		Donya::Vector3 CalcMoveDirection( const Donya::Vector3 moverPos, const Donya::Vector3 &targetPos ) const;
+
+	#if USE_IMGUI
+		void ShowImGuiNode( const std::string &nodeCaption );
+	#endif // USE_IMGUI
+	};
+
+	struct ModelParam
+	{
+		Donya::Model::SkinningModel	model;
+		Donya::Model::MotionHolder	motionHolder;
+	};
+
+
+	class Base
+	{
+	protected:
+		InitializeParam				initializer; // Usually do not change this.
+	protected:
+		Donya::Vector3				pos;
+		Donya::Quaternion			orientation;
+		std::shared_ptr<ModelParam>	pModelParam;
+		Donya::Model::Pose			pose;
+		Donya::Model::Animator		animator;
+	public:
+		Base() = default;
+		virtual ~Base() = default;
+	private:
+		friend class cereal::access;
+		template<class Archive>
+		void serialize( Archive &archive, std::uint32_t version )
+		{
+			archive
+			(
+				CEREAL_NVP( initializer )
+			);
+
+			if ( 1 <= version )
+			{
+				// archive( CEREAL_NVP( x ) );
+			}
+		}
+	public:
+		virtual void Init( const InitializeParam &initializer ) = 0;
+		virtual void Uninit() {}
+
+		virtual void Update( float elapsedTime, const Donya::Vector3 &targetPosition ) = 0;
+		virtual void PhysicUpdate() = 0;
+
+		virtual void Draw( RenderingHelper *pRenderer );
+		virtual void DrawHitBox( RenderingHelper *pRenderer, const Donya::Vector4x4 &matVP );
+	public:
+		const	InitializeParam	&GetInitializer()	const { return initializer; }
+		const	Donya::Vector3	&GetPosition()		const { return pos; }
+	protected:
+		virtual	Donya::Vector4x4 CalcWorldMatrix( bool useForHitBox ) const;
+	};
+
+
+	/// <summary>
+	/// This class moves by following specified direction.
+	/// </summary>
+	class Straight : public Base
+	{
+	private:
+		MoveParam moveParam; // Usually do not change this.
+	private:
+		Donya::Vector3 velocity;
+		float	moveDistanceSum		= 0.0f;
+		bool	nowMoveToPositive	= true;
+	public:
+		Straight() : Base() {}
+	private:
+		friend class cereal::access;
+		template<class Archive>
+		void serialize( Archive &archive, std::uint32_t version )
+		{
+			archive
+			(
+				cereal::base_class<Base>( this ),
+				CEREAL_NVP( moveParam )
+			);
+			if ( 1 <= version )
+			{
+				// archive( CEREAL_NVP( x ) );
+			}
+		}
+	public:
+		void Init( const InitializeParam &initializer ) override;
+
+		void Update( float elapsedTime, const Donya::Vector3 &targetPosition ) override;
+		void PhysicUpdate() override;
+	private:
+		/// <summary>
+		/// This may return not a unit vector.
+		/// </summary>
+		Donya::Vector3 CalcNowMoveDirection( const Donya::Vector3 &targetPosition ) const;
+		void AssignVelocity( const Donya::Vector3 &targetPosition );
+		void AssignOrientation( const Donya::Vector3 &targetPosition );
+	};
 }
+CEREAL_CLASS_VERSION( Enemy::InitializeParam,	0 )
+CEREAL_CLASS_VERSION( Enemy::MoveParam,			0 )
+
+CEREAL_CLASS_VERSION( Enemy::Straight,			0 )
+CEREAL_REGISTER_TYPE( Enemy::Straight			  )
+CEREAL_REGISTER_POLYMORPHIC_RELATION( Enemy::Base, Enemy::Straight )
