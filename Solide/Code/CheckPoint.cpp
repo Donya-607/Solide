5bdc43f0bc15b0b50c836a94a25f70b71717ac21
Donya <alias>
2020-05-04 13:32:53 +0900

    チェックポイント用のクラスを作成

---
+++ Solide/Code/CheckPoint.cpp
@@ -0,0 +1,168 @@
+#include "CheckPoint.h"
+
+#include "Common.h"
+#include "FilePath.h"
+#include "Parameter.h"
+
+void CheckPoint::Init( int stageNumber )
+{
+	stageNo = stageNumber;
+#if DEBUG_MODE
+	LoadJson( stageNumber );
+#else
+	LoadBin( stageNumber );
+#endif // DEBUG_MODE
+}
+void CheckPoint::Uninit() {}
+void CheckPoint::Update( float elapsedTime ) {}
+void CheckPoint::Draw( RenderingHelper *pRenderer, const Donya::Vector4 &color )
+{
+	if ( !pRenderer ) { return; }
+	// else
+
+	// No op.
+}
+void CheckPoint::DrawHitBoxes( RenderingHelper *pRenderer, const Donya::Vector4x4 &matVP, const Donya::Vector4 &color )
+{
+	if ( !Common::IsShowCollision() || !pRenderer ) { return; }
+	// else
+
+	auto DrawCube = [&]( const Donya::AABB &box )
+	{
+		Donya::Vector4x4 W{};
+		W._11 = box.size.x * 2.0f;
+		W._12 = box.size.y * 2.0f;
+		W._13 = box.size.z * 2.0f;
+		W._41 = box.pos.x;
+		W._42 = box.pos.y;
+		W._43 = box.pos.z;
+
+		Donya::Model::Cube::Constant constant;
+		constant.matWorld		= W;
+		constant.matViewProj	= matVP;
+		constant.drawColor		= color;
+		constant.lightDirection	= -Donya::Vector3::Up();
+		pRenderer->ProcessDrawingCube( constant );
+	};
+
+	for ( auto &it : points )
+	{
+		DrawCube( it.hitBox );
+	}
+}
+
+size_t CheckPoint::GetPointCount() const { return points.size(); }
+bool  CheckPoint::IsOutOfRange( size_t index ) const
+{
+	return ( GetPointCount() <= index ) ? true : false;
+}
+const CheckPoint::Instance *CheckPoint::GetPointPtrOrNullptr( size_t index ) const
+{
+	if ( IsOutOfRange( index ) ) { return nullptr; }
+	// else
+	return &points[index];
+}
+
+void CheckPoint::LoadBin ( int stageNo )
+{
+	constexpr bool fromBinary = true;
+	Donya::Serializer::Load( *this, MakeStageParamPath( ID, stageNo, fromBinary ).c_str(), ID, fromBinary );
+}
+void CheckPoint::LoadJson( int stageNo )
+{
+	constexpr bool fromBinary = false;
+	Donya::Serializer::Load( *this, MakeStageParamPath( ID, stageNo, fromBinary ).c_str(), ID, fromBinary );
+}
+#if USE_IMGUI
+void CheckPoint::SaveBin ( int stageNo )
+{
+	constexpr bool fromBinary = true;
+
+	const std::string filePath = MakeStageParamPath( ID, stageNo, fromBinary );
+	MakeFileIfNotExists( filePath, fromBinary );
+
+	Donya::Serializer::Save( *this, filePath.c_str(), ID, fromBinary );
+}
+void CheckPoint::SaveJson( int stageNo )
+{
+	constexpr bool fromBinary = false;
+
+	const std::string filePath = MakeStageParamPath( ID, stageNo, fromBinary );
+	MakeFileIfNotExists( filePath, fromBinary );
+
+	Donya::Serializer::Save( *this, filePath.c_str(), ID, fromBinary );
+}
+void CheckPoint::ShowImGuiNode( const std::string &nodeCaption, int stageNo )
+{
+	if ( !ImGui::TreeNode( nodeCaption.c_str() ) ) { return; }
+	// else
+
+	if ( ImGui::Button( u8"チェックポイントを追加" ) )
+	{
+		points.emplace_back( Instance{} );
+	}
+	if ( 1 < points.size() && ImGui::Button( u8"末尾を削除" ) )
+	{
+		points.pop_back();
+	}
+
+	if ( ImGui::TreeNode( u8"各々の設定" ) )
+	{
+		const size_t pointCount = GetPointCount();
+		size_t eraseIndex = pointCount;
+
+		std::string caption{};
+		for ( size_t i = 0; i < pointCount; ++i )
+		{
+			caption = u8"[" + std::to_string( i ) + u8"]番";
+			if ( ImGui::Button( std::string{ caption + u8"を削除" }.c_str() ) )
+			{
+				eraseIndex = i;
+			}
+
+			ParameterHelper::ShowAABBNode( u8"当たり判定", &points[i].hitBox );
+			points[i].initializer.ShowImGuiNode( caption, stageNo );
+		}
+
+		if ( eraseIndex != pointCount )
+		{
+			points.erase( points.begin() + eraseIndex );
+		}
+
+		ImGui::TreePop();
+	}
+	
+	auto ShowIONode = [&]()
+	{
+		if ( !ImGui::TreeNode( u8"ファイル I/O" ) ) { return; }
+		// else
+
+		const std::string strIndex = u8"[" + std::to_string( stageNo ) + u8"]";
+
+		static bool isBinary = true;
+		if ( ImGui::RadioButton( "Binary", isBinary ) ) { isBinary = true;  }
+		if ( ImGui::RadioButton( "Json",  !isBinary ) ) { isBinary = false; }
+
+		std::string loadStr = u8"ロード" + strIndex;
+		loadStr += u8"(by:";
+		loadStr += ( isBinary ) ? u8"Binary" : u8"Json";
+		loadStr += u8")";
+
+		if ( ImGui::Button( ( u8"セーブ" + strIndex ).c_str() ) )
+		{
+			SaveBin ( stageNo );
+			SaveJson( stageNo );
+		}
+		if ( ImGui::Button( loadStr.c_str() ) )
+		{
+			( isBinary ) ? LoadBin( stageNo ) : LoadJson( stageNo );
+		}
+
+		ImGui::TreePop();
+	};
+	ShowIONode();
+
+	ImGui::TreePop();
+}
+#endif // USE_IMGUI
+
---
+++ Solide/Code/CheckPoint.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <vector>
+
+#include "Donya/Collision.h"
+#include "Donya/Quaternion.h"
+#include "Donya/Serializer.h"
+#include "Donya/UseImGui.h"
+#include "Donya/Vector.h"
+
+#include "Player.h"
+#include "Renderer.h"
+
+class CheckPoint
+{
+public:
+	struct Instance
+	{
+		Donya::AABB			hitBox;
+		PlayerInitializer	initializer;
+	private:
+		friend class cereal::access;
+		template<class Archive>
+		void serialize( Archive &archive, std::uint32_t version )
+		{
+			archive
+			(
+				CEREAL_NVP( hitBox		),
+				CEREAL_NVP( initializer	) 
+			);
+
+			if ( 1 <= version )
+			{
+				// archive( CEREAL_NVP( x ) );
+			}
+		}
+	};
+private: // Serializer member.
+	int						stageNo = 1;
+	std::vector<Instance>	points;
+private:
+	friend class cereal::access;
+	template<class Archive>
+	void serialize( Archive &archive, std::uint32_t version )
+	{
+		archive( CEREAL_NVP( points ) );
+
+		if ( 1 <= version )
+		{
+			// archive( CEREAL_NVP( x ) );
+		}
+	}
+	static constexpr const char *ID = "CheckPoint";
+public:
+	void Init( int stageNo );
+	void Uninit();
+
+	void Update( float elapsedTime );
+
+	void Draw( RenderingHelper *pRenderer, const Donya::Vector4 &color );
+	void DrawHitBoxes( RenderingHelper *pRenderer, const Donya::Vector4x4 &matVP, const Donya::Vector4 &color );
+public:
+	size_t			GetPointCount() const;
+	bool			IsOutOfRange( size_t index ) const;
+	const Instance	*GetPointPtrOrNullptr( size_t index ) const;
+private:
+	void LoadBin( int stageNo );
+	void LoadJson( int stageNo );
+#if USE_IMGUI
+	void SaveBin( int stageNo );
+	void SaveJson( int stageNo );
+public:
+	void ShowImGuiNode( const std::string &nodeCaption, int stageNo );
+#endif // USE_IMGUI
+};
+CEREAL_CLASS_VERSION( CheckPoint, 0 )
