7c62329ebd597ffe0e91679d443a2b4e88c69974
Donya <alias>
2020-06-01 13:42:00 +0900

    タイマクラスを作成

---
+++ Solide/Code/Timer.cpp
@@ -0,0 +1,86 @@
+#include "Timer.h"
+
+#include <algorithm>		// Use std::min
+#include <vector>
+
+#include "Donya/Useful.h"	// Use SeparateDigits().
+
+#undef max
+#undef min
+
+void Timer::Update()
+{
+	current++;
+
+	if ( 60 <= current )
+	{
+		current = 0;
+		second++;
+		if ( 60 <= second )
+		{
+			second = 0;
+			minute++;
+			minute = std::min( 99, minute );
+		}
+	}
+}
+
+void Timer::Set( int newMinute, int newSecond, int newCurrent )
+{
+	if ( 0 <= newMinute  ) { minute  = newMinute;  }
+	if ( 0 <= newSecond  ) { second  = newSecond;  }
+	if ( 0 <= newCurrent ) { current = newCurrent; }
+}
+
+std::string Timer::ToStr( bool isInsertColon )
+{
+	constexpr int		DIGIT = 2;
+	constexpr size_t	SIZE  = 3;
+	const std::vector<int> SEPARATED_TIMES // [0:current][1:second][2:minute]
+	{
+		Current(),
+		Second(),
+		Minute()
+	};
+
+	std::vector<int> separatedNumbers{};
+
+	for ( size_t i = 0; i < SIZE; ++i )
+	{
+		auto result = Donya::SeparateDigits( SEPARATED_TIMES[i], DIGIT );
+		for ( const auto &it : result )
+		{
+			separatedNumbers.emplace_back( it );
+		}
+	}
+
+	std::string reverseNumberStr{}; // Store "123456" to "65:43:21".
+
+	const size_t numberCount = separatedNumbers.size();
+	for ( size_t i = 0; i < numberCount; ++i )
+	{
+		reverseNumberStr += std::to_string( separatedNumbers[i] );
+
+		bool canInsert = ( isInsertColon && i + 1 < numberCount );
+		if ( i % DIGIT == 1 && canInsert )
+		{
+			reverseNumberStr += ":";
+		}
+	}
+
+	std::reverse( reverseNumberStr.begin(), reverseNumberStr.end() );
+	return reverseNumberStr;
+}
+
+bool operator < ( const Timer &L, const Timer &R )
+{
+	if ( L.Minute()  < R.Minute()  ) { return true;  }
+	if ( L.Minute()  > R.Minute()  ) { return false; }
+	// else
+	if ( L.Second()  < R.Second()  ) { return true;  }
+	if ( L.Second()  > R.Second()  ) { return false; }
+	// else
+	if ( L.Current() < R.Current() ) { return true;  }
+	// else
+	return false;
+}
---
+++ Solide/Code/Timer.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+
+#include "Donya/Serializer.h"
+
+/// <summary>
+/// Measure elapsed time. this class can count until 99min-59sec-59ms with frame.
+/// </summary>
+class Timer
+{
+private:
+	int current	= 59;	// 0 ~ 59.
+	int second	= 59;	// 0 ~ 59.
+	int minute	= 99;	// 0 ~ 99.
+private:
+	friend class cereal::access;
+	template<class Archive>
+	void serialize( Archive &archive, std::uint32_t version )
+	{
+		archive
+		(
+			CEREAL_NVP( current ),
+			CEREAL_NVP( second  ),
+			CEREAL_NVP( minute  )
+		);
+
+		if ( 1 <= version )
+		{
+			// archive( CEREAL_NVP() );
+		}
+	}
+public:
+	void Update();
+public:
+	/// <summary>
+	/// If setting -1, that parameter is not change.
+	/// </summary>
+	void Set( int newMinute = -1, int newSecond = -1, int newCurrent = -1 );
+public:
+	int Current() const { return current; }
+	int Second()  const { return second;  }
+	int Minute()  const { return minute;  }
+	/// <summary>
+	/// Returns string is "XX:XX:XX", min:sec:ms.
+	/// </summary>
+	std::string ToStr( bool isInsertColon = true );
+};
+CEREAL_CLASS_VERSION( Timer, 0 )
+
+static bool operator <  ( const Timer &L, const Timer &R );
+static bool operator >  ( const Timer &L, const Timer &R ) { return R < L; }
+static bool operator <= ( const Timer &L, const Timer &R ) { return !( R < L ); }
+static bool operator >= ( const Timer &L, const Timer &R ) { return !( L < R ); }
+
+static bool operator == ( const Timer &L, const Timer &R ) { return !( L < R ) && !( R > L ); }
+static bool operator != ( const Timer &L, const Timer &R ) { return !( L == R ); }
