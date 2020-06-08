884025557dfe5a30af6aa1daef6468fe0e3db633
Donya <alias>
2020-06-08 22:29:32 +0900

    モーションが変わるたびにタイマをリセットするように

--- Solide/Code/Boss.cpp
+++ Solide/Code/Boss.cpp
@@ -1496,6 +1496,7 @@ void BossFirst::MotionManager::ApplyMotion( BossFirst &inst, MotionKind kind )
 
 	currentKind = kind;
 
+	inst.model.animator.ResetTimer();
 	inst.AssignSpecifyPose( intKind );
 
 	ApplyLoopFlag( inst, kind );
